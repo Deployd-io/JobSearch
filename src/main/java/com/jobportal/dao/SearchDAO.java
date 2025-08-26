691cb49511520e4c768e742a528a068b1f3724f9
Ashish Butani <ashish.butani@deployd.io>
2025-08-26 17:50:24 -0400

    Initial commit - copied from source-repo

---
+++ src/main/java/com/jobportal/dao/SearchDAO.java
@@ -0,0 +1,33 @@
+package com.jobportal.dao;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.TextCriteria;
+import org.springframework.data.mongodb.core.query.TextQuery;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public class SearchDAO {
+	
+	@Autowired
+	MongoTemplate template;
+
+	public <T> List<T> searchByTerm(Class className, String term, Pageable p)
+	{
+		TextCriteria criteria = TextCriteria.forDefaultLanguage()
+				.matchingAny(term.split("\\s+"));  // handles multiple words
+
+		Query query = TextQuery.queryText(criteria)
+				.with(Sort.by(Sort.Direction.DESC, "updatedOn"))
+				.with(p);
+
+		return template.find(query, className);
+	}
+}
