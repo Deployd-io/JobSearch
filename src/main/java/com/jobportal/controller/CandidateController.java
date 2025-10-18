e65ab191644d50144e9a63d2f2b803f6d18f0e92
root <root@ubuntu-24.localhost>
2025-10-18 01:00:21 +0000

    Logs added by LogWrite

--- src/main/java/com/jobportal/controller/CandidateController.java
+++ src/main/java/com/jobportal/controller/CandidateController.java
@@ -17,10 +17,12 @@ import org.springframework.web.bind.annotation.RestController;
 import com.jobportal.dto.CandidateDTO;
 import com.jobportal.service.CandidateService;
 import com.jobportal.service.SearchService;
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequestMapping("/candidates")
 @CrossOrigin("*")
+@Slf4j
 public class CandidateController {
 	
 	@Autowired
@@ -33,6 +35,8 @@ public class CandidateController {
 	@GetMapping
 	public List<CandidateDTO> findAll()
 	{
+		log.debug(">>> Entering findAll()");
+		log.debug("<<< Exiting findAll()");
 		return candidateService.findAll();
 	}
 	
@@ -45,18 +49,24 @@ public class CandidateController {
 	@PostMapping
 	public String createCandidate(@RequestBody CandidateDTO cndtDTO)
 	{
+		log.debug(">>> Entering createCandidate(cndtDTO={})", cndtDTO);
+		log.debug("<<< Exiting createCandidate(cndtDTO={})", cndtDTO);
 		return candidateService.createCandidate(cndtDTO);
 	}
 	
 	@PutMapping
 	public void updateCandidate(@RequestBody CandidateDTO cndtDTO)
 	{
+		log.debug(">>> Entering updateCandidate(cndtDTO={})", cndtDTO);
 		candidateService.updateCandidate(cndtDTO);
+		log.debug("<<< Exiting updateCandidate(cndtDTO={})", cndtDTO);
 	}
 	
 	@GetMapping(value = "/search")
 	public List<CandidateDTO> search(@RequestParam String term, Pageable p)
 	{
+		log.debug(">>> Entering search(term={},p={})", term, p);
+		log.debug("<<< Exiting search(term={},p={})", term, p);
 		return searchService.searchCandidatesByTerm(term, p);
 	}
  
