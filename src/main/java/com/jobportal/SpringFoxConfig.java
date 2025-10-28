e2a1657d87510a62471fd88bd5d9d2d6abba4817
root <root@ubuntu-24.localhost>
2025-10-28 19:39:53 +0000

    Logs added by LogWrite

--- src/main/java/com/jobportal/SpringFoxConfig.java
+++ src/main/java/com/jobportal/SpringFoxConfig.java
@@ -8,12 +8,16 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import lombok.extern.slf4j.Slf4j;
 
 @Configuration
 @EnableSwagger2
+@Slf4j
 public class SpringFoxConfig {                                    
     @Bean
     public Docket api() { 
+		log.debug(">>> Entering api()");
+		log.debug("<<< Exiting api()");
         return new Docket(DocumentationType.SWAGGER_2)  
           .select()                                  
           .apis(RequestHandlerSelectors.any())              
