5e537dea1fe5e73e1d0379a90ba4f35444b72e9c
Ashish Butani <ashish.butani@deployd.io>
2025-11-27 20:37:44 -0500

    updated plugin files

--- logrite-plugin/gatekeeper.go
+++ logrite-plugin/gatekeeper.go
@@ -21,7 +21,7 @@ type Request struct {
 }
 
 type Response struct {
-    ScanID      int    `json:"scanId"`
+    PRLink  string    `json:"prLink"`
     Status  string `json:"status"`
     Message string `json:"message"`
 }
@@ -95,27 +95,13 @@ func main() {
     var apiResp Response
     json.NewDecoder(res.Body).Decode(&apiResp)
 
+    fmt.Println("STATUS:", apiResp.Status)
     fmt.Println("API RESPONSE:", apiResp.Message)
+    fmt.Println("PR Link:", apiResp.PRLink)
 
-    if apiResp.Status == "allow" {
-    	os.Exit(0)
-    } else {
-    	fmt.Printf("❌ Coverage check failed! Scan ID: %d\n", apiResp.ScanID)
+    if apiResp.Status != "allow" {
     	fmt.Printf("❌ Coverage failed! Fixing automatically...\n")
-
-	fmt.Printf("pr_link=%s\n", "test.com")
-    	// Call your API to fix the issue
-    	// fixResp, err := callFixAPI(apiResp.ScanID) // implement this function
-    	//if err != nil {
-        //	fmt.Println("API ERROR while fixing:", err)
-        //	os.Exit(1)
-    	//}
-
-    	// fixResp should contain the PR link
-    	// fmt.Printf("✅ Fix applied! Review the PR here: %s\n", fixResp.PRLink)
-
-    	os.Exit(1)
+    	fmt.Println("PR_LINK:", apiResp.PRLink)
     }
-
 }
 
