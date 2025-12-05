e3790882fe2fbd0c92e8be05528d089073e0d2c3
Ashish Butani <ashish.butani@deployd.io>
2025-12-04 20:27:28 -0500

    perform cleanup

--- src/main/java/com/jobportal/service/CandidateService.java
+++ src/main/java/com/jobportal/service/CandidateService.java
@@ -18,65 +18,61 @@ import com.jobportal.model.Candidate;
 @Service
 public class CandidateService {
 
-	@Autowired
-	CandidateDAO dao;
-	
-	@Autowired
-	ModelMapper modelMapper;
-	
-	@Autowired
-	ModelMapperService modelMapperService;
-	
-	
-	public List<CandidateDTO> findAll()
-	{
-		return dao.findAll().stream().map(cndt -> 
-			modelMapper.map(cndt, CandidateDTO.class)).collect(Collectors.toList());
-	}
-	
-	public CandidateDTO findById(String id)
-	{
-		Optional<Candidate> optCndt = dao.findById(id);
-		
-		if (!optCndt.isPresent())
-			return null;
-		
-		return modelMapper.map(optCndt.get(), CandidateDTO.class);
-	}
-	
-	@Transactional
-	public String createCandidate(CandidateDTO cndtDTO)
-	{
-		Candidate cndt = modelMapper.map(cndtDTO, Candidate.class);
-		cndt.setCreatedOn((new Date()).toString());
-		cndt.setUpdatedOn(cndt.getCreatedOn());
-		
-		Point point = new Point(cndtDTO.getLng(), cndtDTO.getLat());
-		cndt.setPoint(point);
-		
-		dao.save(cndt);
-		
-		return cndt.getCandidateId();
-	}
-	
-	
-	@Transactional
-	public void updateCandidate(CandidateDTO cndtDTO)
-	{
-		Optional<Candidate> optCndt = dao.findById(cndtDTO.getCandidateId());
-		
-		if (!optCndt.isPresent())
-			return;
-		
-		Candidate cndt = optCndt.get();
-		cndt.setUpdatedOn((new Date()).toString());
-		
-		Point point = new Point(cndtDTO.getLng(), cndtDTO.getLat());
-		cndt.setPoint(point);
-		
-		modelMapperService.getNonNullModelMapper().map(cndtDTO, cndt);
-		
-		dao.save(cndt);
-	}
+    @Autowired
+    CandidateDAO dao;
+
+    @Autowired
+    ModelMapper modelMapper;
+
+    @Autowired
+    ModelMapperService modelMapperService;
+
+
+    public List<CandidateDTO> findAll() {
+        return dao.findAll().stream().map(cndt ->
+                modelMapper.map(cndt, CandidateDTO.class)).collect(Collectors.toList());
+    }
+
+    public CandidateDTO findById(String id) {
+        Optional<Candidate> optCndt = dao.findById(id);
+
+        if (!optCndt.isPresent())
+            return null;
+
+        return modelMapper.map(optCndt.get(), CandidateDTO.class);
+    }
+
+    @Transactional
+    public String createCandidate(CandidateDTO cndtDTO) {
+        Candidate cndt = modelMapper.map(cndtDTO, Candidate.class);
+        cndt.setCreatedOn((new Date()).toString());
+        cndt.setUpdatedOn(cndt.getCreatedOn());
+
+        Point point = new Point(cndtDTO.getLng(), cndtDTO.getLat());
+        cndt.setPoint(point);
+
+        dao.save(cndt);
+
+        return cndt.getCandidateId();
+    }
+
+
+    @Transactional
+    public void updateCandidate(CandidateDTO cndtDTO) {
+        Optional<Candidate> optCndt = dao.findById(cndtDTO.getCandidateId());
+
+        if (!optCndt.isPresent())
+            return;
+
+        Candidate cndt = optCndt.get();
+        cndt.setUpdatedOn((new Date()).toString());
+
+        Point point = new Point(cndtDTO.getLng(), cndtDTO.getLat());
+        cndt.setPoint(point);
+
+        modelMapperService.getNonNullModelMapper().map(cndtDTO, cndt);
+
+        dao.save(cndt);
+    }
 
 }
