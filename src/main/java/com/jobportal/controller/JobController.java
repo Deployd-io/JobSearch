3d14d10eb177fd3b7c9dbbcad9a4f8b3ed7fa7e7
Ashish Butani <ashish.butani@deployd.io>
2025-12-04 20:29:33 -0500

    perform cleanup

--- src/main/java/com/jobportal/controller/JobController.java
+++ src/main/java/com/jobportal/controller/JobController.java
@@ -71,7 +71,6 @@ public class JobController {
 	public void updateJob(@RequestBody JobDTO jobDTO)
 	{
 		String testValue = "Testing my local changes";
-		testValue = "value changed, needs logging";
 
 		jobService.updateJob(jobDTO);
 	}
