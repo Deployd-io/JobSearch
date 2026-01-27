7baf56f5c6e5eef9f96eb57690688151a77071f6
root <root@ubuntu-24.localhost>
2026-01-27 16:17:09 +0000

    Logs added by LogRite

--- src/main/java/com/jobportal/MongoJsonDataLoader.java
+++ src/main/java/com/jobportal/MongoJsonDataLoader.java
@@ -11,25 +11,33 @@ import org.springframework.stereotype.Component;
 import javax.annotation.PostConstruct;
 import java.io.InputStream;
 import java.util.List;
+import lombok.extern.slf4j.Slf4j;
 
 @Component
+@Slf4j
 public class MongoJsonDataLoader {
 
     private final MongoTemplate mongoTemplate;
     private final ObjectMapper objectMapper;
 
     public MongoJsonDataLoader(MongoTemplate mongoTemplate, ObjectMapper objectMapper) {
+		log.debug(">>> Entering MongoJsonDataLoader(mongoTemplate={},objectMapper={})", mongoTemplate, objectMapper);
         this.mongoTemplate = mongoTemplate;
         this.objectMapper = objectMapper;
+		log.debug("MongoJsonDataLoader(mongoTemplate={},objectMapper={}): mongoTemplate → {}", mongoTemplate, objectMapper, mongoTemplate);
+		log.debug("MongoJsonDataLoader(mongoTemplate={},objectMapper={}): objectMapper → {}", mongoTemplate, objectMapper, objectMapper);
+		log.debug("<<< Exiting MongoJsonDataLoader(mongoTemplate={},objectMapper={})", mongoTemplate, objectMapper);
     }
 
     @PostConstruct
     public void loadData() throws Exception {
+		log.debug(">>> Entering loadData()");
         if (mongoTemplate.getCollection("job").countDocuments() > 0) {
             return;
         }
 
         InputStream is = new ClassPathResource("jobs.json").getInputStream();
+		log.debug("loadData(): is → {}", is);
         List<Job> jobs = objectMapper.readValue(is, new TypeReference<List<Job>>() {});
         mongoTemplate.insert(jobs, Job.class);
     }
