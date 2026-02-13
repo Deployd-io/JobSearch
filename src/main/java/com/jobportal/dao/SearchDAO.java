d4fe163d7d1429733412fcdc389a729e3734233e
root <root@ubuntu-24.localhost>
2026-02-13 12:55:31 +0000

    Logs added by LogRite

--- src/main/java/com/jobportal/dao/SearchDAO.java
+++ src/main/java/com/jobportal/dao/SearchDAO.java
@@ -19,12 +19,12 @@ public class SearchDAO {
 	@Autowired
 	MongoTemplate template;
 
-	public <T> List<T> searchByTerm(Class className, String term, Pageable p)
+	public <T> List<T> searchByTerm(Class<T> className, String term, Pageable p)
 	{
 		TextCriteria criteria = TextCriteria.forDefaultLanguage()
 				.matchingAny(term.split("\\s+"));  // handles multiple words
 
-		Query query = new Query()
+		Query query = TextQuery.queryText(criteria)
 				.with(Sort.by(Sort.Direction.DESC, "updatedOn"))
 				.with(p);
 
