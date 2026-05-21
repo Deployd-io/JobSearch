141df457d23b478b09567a9eae7b2e268b5ce31c
root <root@b0186218be52>
2026-05-21 08:25:20 +0000

    Logs added by LogRite

--- src/main/java/com/jobportal/MongoJsonDataLoader.java
+++ src/main/java/com/jobportal/MongoJsonDataLoader.java
@@ -11,27 +11,40 @@ import org.springframework.stereotype.Component;
 import javax.annotation.PostConstruct;
 import java.io.InputStream;
 import java.util.List;
+import lombok.extern.slf4j.Slf4j;
 
 @Component
+@Slf4j
 public class MongoJsonDataLoader {
 
     private final MongoTemplate mongoTemplate;
     private final ObjectMapper objectMapper;
 
     public MongoJsonDataLoader(MongoTemplate mongoTemplate, ObjectMapper objectMapper) {
+		log.debug(">>> Entering MongoJsonDataLoader(mongoTemplate={},objectMapper={})", mongoTemplate, objectMapper);
         this.mongoTemplate = mongoTemplate;
         this.objectMapper = objectMapper;
+		log.debug("<<< Exiting MongoJsonDataLoader(mongoTemplate={},objectMapper={})", mongoTemplate, objectMapper);
+		log.debug("MongoJsonDataLoader(mongoTemplate={},objectMapper={}): this.mongoTemplate → {}", mongoTemplate, objectMapper, this.mongoTemplate);
+		log.debug("MongoJsonDataLoader(mongoTemplate={},objectMapper={}): objectMapper → {}", mongoTemplate, objectMapper, objectMapper);
+		log.debug("MongoJsonDataLoader(mongoTemplate={},objectMapper={}): this.objectMapper → {}", mongoTemplate, objectMapper, this.objectMapper);
+		log.debug("MongoJsonDataLoader(mongoTemplate={},objectMapper={}): mongoTemplate → {}", mongoTemplate, objectMapper, mongoTemplate);
     }
 
     @PostConstruct
     public void loadData() throws Exception {
+		log.debug(">>> Entering loadData()");
         if (mongoTemplate.getCollection("job").countDocuments() > 0) {
+			log.debug("MongoJsonDataLoader(mongoTemplate={},objectMapper={}): mongoTemplate → {}", mongoTemplate, objectMapper, mongoTemplate);
             return;
         }
 
         InputStream is = new ClassPathResource("jobs.json").getInputStream();
+		log.debug("loadData(): jobs → {}", jobs);
+		log.debug("loadData(): is → {}", is);
         List<Job> jobs = objectMapper.readValue(is, new TypeReference<List<Job>>() {});
         mongoTemplate.insert(jobs, Job.class);
+		log.debug("<<< Exiting loadData()");
     }
 }
 
