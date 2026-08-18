2a145af2c0fcbbd5b62dc2112ccea5f286f1c302
root <root@f1ab72198e9c>
2026-08-18 06:19:17 +0000

    Logs added by LogRite

--- src/main/java/com/jobportal/MongoJsonDataLoader.java
+++ src/main/java/com/jobportal/MongoJsonDataLoader.java
@@ -11,27 +11,39 @@ import org.springframework.stereotype.Component;
 import javax.annotation.PostConstruct;
 import java.io.InputStream;
 import java.util.List;
+import lombok.extern.slf4j.Slf4j;
 
 @Component
+@Slf4j
 public class MongoJsonDataLoader {
 
     private final MongoTemplate mongoTemplate;
     private final ObjectMapper objectMapper;
 
     public MongoJsonDataLoader(MongoTemplate mongoTemplate, ObjectMapper objectMapper) {
+		log.debug(">>> Entering MongoJsonDataLoader(mongoTemplate={},objectMapper={})", mongoTemplate, objectMapper);
         this.mongoTemplate = mongoTemplate;
+		log.debug("MongoJsonDataLoader(mongoTemplate={},objectMapper={}): this.mongoTemplate → {}", mongoTemplate, objectMapper, this.mongoTemplate);
         this.objectMapper = objectMapper;
+		log.debug("<<< Exiting MongoJsonDataLoader(mongoTemplate={},objectMapper={})", mongoTemplate, objectMapper);
     }
 
     @PostConstruct
     public void loadData() throws Exception {
+		log.debug(">>> Entering loadData()");
+		long start = System.currentTimeMillis();
         if (mongoTemplate.getCollection("job").countDocuments() > 0) {
+			log.info("loadData(): find query executed in {} ms", (System.currentTimeMillis() - start));
             return;
         }
 
+		log.info("loadData(): save query executed in {} ms", (System.currentTimeMillis() - start));
         InputStream is = new ClassPathResource("jobs.json").getInputStream();
+		log.debug("loadData(): is → {}", is);
         List<Job> jobs = objectMapper.readValue(is, new TypeReference<List<Job>>() {});
+		log.debug("loadData(): jobs → {}", jobs);
         mongoTemplate.insert(jobs, Job.class);
+		log.debug("<<< Exiting loadData()");
     }
 }
 
